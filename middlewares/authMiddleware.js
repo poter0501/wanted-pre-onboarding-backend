8a2fcc244fbedd7c2fe8632d6a9a75b4bbf6b568
Ubuntu <ubuntu@ip-172-31-34-245.ap-northeast-2.compute.internal>
2023-08-06 05:47:55 +0000

    Feat: 토큰 인증 middleware 코드 수정

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers['authorization'];
+    let token = req.headers['authorization'] || req.headers['Authorization'];
 
     if (!token) {
         return res.status(403).json({ message: "No token provided" });
+    }
+
+    // "Bearer " 접두사 제거
+    if (token.startsWith('Bearer ')) {
+        token = token.slice(7, token.length);
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
