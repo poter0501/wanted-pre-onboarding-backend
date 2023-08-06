eef705e997f1b56e1e334c1ca3a4f5fca8a7154d
Ubuntu <ubuntu@ip-172-31-34-245.ap-northeast-2.compute.internal>
2023-08-06 08:12:21 +0000

    Test: 게시글 목록 조회(Pagenation) test 코드 작성

--- __test__/articleController.test.js
+++ __test__/articleController.test.js
@@ -14,22 +14,22 @@ describe('articleController.post', () => {
             userId: 1
         });
         const res = httpMocks.createResponse();
-    
+
         const mockArticle = { id: 1, author: 'testAuthor', content: 'testContent' };
         Article.create.mockResolvedValue(mockArticle);
-    
+
         const expectedResponse = {
             message: "Article created successfully",
             article: mockArticle
         };
-    
+
         await articleController.post(req, res);
-    
+
         const receivedResponse = JSON.parse(res._getData()); // 응답을 객체로 변환
 
         expect(res.statusCode).toBe(201);
         expect(receivedResponse).toEqual(expectedResponse);
-    });    
+    });
 
     it('should return 500 on error', async () => {
         const req = httpMocks.createRequest({
@@ -48,3 +48,95 @@ describe('articleController.post', () => {
         expect(res.statusCode).toBe(500);
     });
 });
+describe('articleController.list', () => {
+
+    let req, res, mockArticles;
+
+    beforeAll(() => {
+        // 13개의 모의 Article 생성
+        mockArticles = Array.from({ length: 13 }).map((_, i) => {
+            return { 
+                id: 13 - i, 
+                author: `testAuthor${13 - i}`, 
+                content: `testContent${13 - i}` 
+            };
+        });
+    });
+
+    beforeEach(() => {
+        req = httpMocks.createRequest();
+        res = httpMocks.createResponse();
+    });
+
+    it('should fetch the latest 5 paginated articles from a set of 13 and return 200', async () => {
+        req.query.page = '1';
+        req.query.limit = '5';
+
+        // 첫 페이지에 대한 응답 설정: 가장 최근의 5개 Article 반환
+        const mockResponse = {
+            count: 13,
+            rows: mockArticles.slice(0, 5)
+        };
+
+        Article.findAndCountAll.mockResolvedValue(mockResponse);
+
+        await articleController.list(req, res);
+        const responseData = JSON.parse(res._getData());
+
+        const expectedResponse = {
+            articles: mockArticles.slice(0, 5),
+            pagination: {
+                totalItems: 13,
+                itemsPerPage: 5,
+                currentPage: 1,
+                totalPages: 3
+            }
+        };
+
+        expect(res.statusCode).toBe(200);
+        expect(responseData).toEqual(expectedResponse);
+    });
+    it('should fetch the last 3 articles when the 3rd page is requested from a set of 13', async () => {
+        req.query.page = '3';
+        req.query.limit = '5';
+    
+        // 세 번째 페이지에 대한 응답 설정: Article 1,2,3 반환
+        const mockResponse = {
+            count: 13,
+            rows: mockArticles.slice(10, 13)
+        };
+    
+        Article.findAndCountAll.mockResolvedValue(mockResponse);
+    
+        await articleController.list(req, res);
+        const responseData = JSON.parse(res._getData());
+    
+        const expectedResponse = {
+            articles: mockArticles.slice(10, 13),
+            pagination: {
+                totalItems: 13,
+                itemsPerPage: 5,
+                currentPage: 3,
+                totalPages: 3
+            }
+        };
+    
+        expect(res.statusCode).toBe(200);
+        expect(responseData).toEqual(expectedResponse);
+    });
+
+    it('should return 500 if there is an error', async () => {
+        Article.findAndCountAll.mockRejectedValue(new Error('Test error'));
+
+        await articleController.list(req, res);
+        const responseData = JSON.parse(res._getData()); // 응답을 객체로 변환
+
+        const expectedErrorResponse = {
+            message: 'Error fetching articles',
+            error: 'Test error'
+        };
+
+        expect(res.statusCode).toBe(500);
+        expect(responseData).toEqual(expectedErrorResponse);
+    });
+})
