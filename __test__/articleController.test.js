8d59e3c0c6114c6e8e5ad40ddaec123cd90bca94
Ubuntu <ubuntu@ip-172-31-34-245.ap-northeast-2.compute.internal>
2023-08-06 10:43:12 +0000

    Test: 특정 게시글을 수정하는 기능 test 코드 작성

--- __test__/articleController.test.js
+++ __test__/articleController.test.js
@@ -55,10 +55,10 @@ describe('articleController.list', () => {
     beforeAll(() => {
         // 13개의 모의 Article 생성
         mockArticles = Array.from({ length: 13 }).map((_, i) => {
-            return { 
-                id: 13 - i, 
-                author: `testAuthor${13 - i}`, 
-                content: `testContent${13 - i}` 
+            return {
+                id: 13 - i,
+                author: `testAuthor${13 - i}`,
+                content: `testContent${13 - i}`
             };
         });
     });
@@ -99,18 +99,18 @@ describe('articleController.list', () => {
     it('should fetch the last 3 articles when the 3rd page is requested from a set of 13', async () => {
         req.query.page = '3';
         req.query.limit = '5';
-    
+
         // 세 번째 페이지에 대한 응답 설정: Article 1,2,3 반환
         const mockResponse = {
             count: 13,
             rows: mockArticles.slice(10, 13)
         };
-    
+
         Article.findAndCountAll.mockResolvedValue(mockResponse);
-    
+
         await articleController.list(req, res);
         const responseData = JSON.parse(res._getData());
-    
+
         const expectedResponse = {
             articles: mockArticles.slice(10, 13),
             pagination: {
@@ -120,7 +120,7 @@ describe('articleController.list', () => {
                 totalPages: 3
             }
         };
-    
+
         expect(res.statusCode).toBe(200);
         expect(responseData).toEqual(expectedResponse);
     });
@@ -140,7 +140,7 @@ describe('articleController.list', () => {
         expect(responseData).toEqual(expectedErrorResponse);
     });
 });
-describe('getArticleById', () => {
+describe('articleController.getArticleById', () => {
     let req, res;
 
     beforeEach(() => {
@@ -178,14 +178,88 @@ describe('getArticleById', () => {
     it('should return 500 if there is an error', async () => {
         Article.findByPk.mockRejectedValue(new Error('Test error'));
 
-        req.params.id = '1'; 
+        req.params.id = '1';
 
         await articleController.getArticleById(req, res);
 
         const responseData = res._getData();
-        
+
         expect(res.statusCode).toBe(500);
         expect(responseData.message).toBe('Error retrieving article');
         expect(responseData.error).toBe('Test error');
+    });
+});
+describe('articleController.update', () => {
+    let req, res;
+    const mockArticleId = 1;
+    const mockUserId = 1;
+    const mockContent = 'Updated Content';
+
+    beforeEach(() => {
+        req = httpMocks.createRequest({
+            params: { id: mockArticleId },
+            userId: mockUserId,
+            body: { content: mockContent }
+        });
+        res = httpMocks.createResponse();
+    });
+
+    it('should update the article and return 200', async () => {
+        const mockArticle = {
+            id: mockArticleId,
+            author: mockUserId,
+            content: mockContent,
+            save: jest.fn()
+        };
+
+        Article.findByPk.mockResolvedValue(mockArticle);
+
+        await articleController.update(req, res);
+
+        expect(mockArticle.save).toHaveBeenCalled();
+        expect(res.statusCode).toBe(200);
+        const responseData = JSON.parse(res._getData());
+        expect(responseData.article.author).toEqual(mockArticle.author);
+        expect(responseData.article.content).toEqual(mockArticle.content);
+        expect(responseData.article.id).toEqual(mockArticle.id);
+
+    });
+
+    it('should return 404 if the article is not found', async () => {
+        Article.findByPk.mockResolvedValue(null);
+
+        await articleController.update(req, res);
+
+        expect(res.statusCode).toBe(404);
+        const responseData = JSON.parse(res._getData());
+        expect(responseData).toEqual({ message: "Article not found" });
+    });
+
+    it('should return 403 if user is not the author', async () => {
+        const mockArticle = {
+            id: mockArticleId,
+            author: mockUserId + 1,
+            content: mockContent,
+            save: jest.fn()
+        };
+
+        Article.findByPk.mockResolvedValue(mockArticle);
+
+        await articleController.update(req, res);
+
+        expect(res.statusCode).toBe(403);
+        const responseData = JSON.parse(res._getData());
+        expect(responseData).toEqual({ message: "You are not authorized to edit this article" });
+    });
+
+    it('should return 500 if there is an error', async () => {
+        Article.findByPk.mockRejectedValue(new Error('Test error'));
+
+        await articleController.update(req, res);
+
+        const responseData = JSON.parse(res._getData());
+        console.log(`${responseData}`);
+        expect(res.statusCode).toBe(500);
+        expect(responseData).toEqual({ message: "Error updating article", error: "Test error" });
     });
 });
